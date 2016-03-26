3261589b8ad8e83f94f684bec30ead4b5f2ddb03
Jay.M.Hu <jay.m.hu@newegg.com>
2016-03-26 17:44:17 +0800

    Add resources.

--- gulpfile.js
+++ gulpfile.js
@@ -3,24 +3,35 @@
 var gulp = require('gulp');
 var del = require('del');
 var browserSync = require('browser-sync').create();
+var concat = require('gulp-concat');
 
-gulp.task('serve', () => {
+gulp.task('serve', (done) => {
   browserSync.init({
     server: {
       baseDir: './dist'
     },
     ghostMode: false,
     port: 7410
-  })
+  });
+  done();
 });
 
-gulp.task('copy', () =>
+gulp.task('copySrc', () =>
   gulp.src('./src/**')
     .pipe(gulp.dest('./dist'))
 );
 
+gulp.task('copyAssets', () =>
+  gulp.src('./assets/**')
+    .pipe(gulp.dest('./dist/assets/'))
+);
+
+gulp.task('watch', () => {
+  return gulp.watch(['src/**/*', 'assets/**/*'], gulp.series('copyAssets', 'copySrc', () => browserSync.reload()))
+});
+
 //开发
-gulp.task('dev', gulp.series('copy', 'serve'));
+gulp.task('dev', gulp.series('copyAssets', 'copySrc', 'serve', 'watch'));
 
 //发布
 gulp.task('publish', gulp.series('dev'));
